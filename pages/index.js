e400d5922f1243955255ad03183ab2ead6cd5a2c
Ricardo <ricardo.a.nava@gmail.com>
2020-07-16 14:53:19 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Decantos Menu</title>
+      </head>
+      <body class="bg-gray-200">
+        <header>Header</header>
+        <main>Main</main>
+        <footer>Footer</footer>
+      </body>
+    </html>
+  );
+}

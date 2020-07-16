39f5073fe81a6f1566c8a25c211ec5bac9f05528
Ricardo <ricardo.a.nava@gmail.com>
2020-07-16 15:08:49 -0700

    Implement camelCase usage for properties

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Decantos Menu</title>
       </head>
       <body className="bg-gray-200">

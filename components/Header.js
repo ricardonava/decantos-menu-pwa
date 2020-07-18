a0f92dba2fe94448295c5b4b32fc146ec032e4ec
Ricardo <ricardo.a.nava@gmail.com>
2020-07-17 20:11:23 -0700

    Add InstallPWA component for PWA IOS compatibility

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import InstallPWA from './InstallPWA';
+
 export default function Header() {
   return (
     <header className="flex">
@@ -7,6 +9,7 @@ export default function Header() {
         alt="DECANTOS"
         src="https://static.wixstatic.com/media/6ad50f_e5a06ecb811d44929bb9bf96fe327ad6~mv2.jpg/v1/fill/w_132,h_120,al_c,q_80,usm_0.66_1.00_0.01/LISTA%20DE%20VINO%202020-01.webp"
       />
+      <InstallPWA />
     </header>
   );
 }

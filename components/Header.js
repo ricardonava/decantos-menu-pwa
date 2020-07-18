a8dcd1a7d01ff78828a7c6f297a7a8e047571d56
Ricardo <ricardo.a.nava@gmail.com>
2020-07-17 22:50:42 -0700

    Add static logo

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,7 @@ import InstallPWA from './InstallPWA';
 export default function Header() {
   return (
     <header className="flex">
-      <img
-        className="mx-auto mb-6"
-        alt="DECANTOS"
-        src="https://static.wixstatic.com/media/6ad50f_e5a06ecb811d44929bb9bf96fe327ad6~mv2.jpg/v1/fill/w_132,h_120,al_c,q_80,usm_0.66_1.00_0.01/LISTA%20DE%20VINO%202020-01.webp"
-      />
+      <img className="mx-auto mb-6" alt="DECANTOS" src="/images/decantos.jpg" />
       <InstallPWA />
     </header>
   );

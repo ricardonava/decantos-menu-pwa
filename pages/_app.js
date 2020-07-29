6185ee8194523ae966e32c2862d38342977a416d
Ricardo <ricardo.a.nava@gmail.com>
2020-07-29 14:31:53 -0700

    Make user-scalable true for accesibility

--- pages/_app.js
+++ pages/_app.js
@@ -51,7 +51,7 @@ function App({ Component, pageProps }) {
         {/* <!-- Fitscreen  --> */}
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=yes"
         />
 
         {/* <!-- Layout mode --> */}

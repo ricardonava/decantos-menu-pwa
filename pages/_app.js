db33220e692c8c0e7942557d75e9242c900b8a2f
Ricardo <ricardo.a.nava@gmail.com>
2020-07-29 14:34:04 -0700

    Add user-scalable on viewport

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function App({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=yes"
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />

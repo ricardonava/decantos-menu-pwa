faf1307e1ecef2548b716a3c5bfb1a4fbb3761a5
Ricardo <ricardo.a.nava@gmail.com>
2020-07-30 19:50:33 -0700

    Refactor nav buttons to add icons and rm text

--- components/Nav.js
+++ components/Nav.js
@@ -16,12 +16,24 @@ export default function Nav() {
         <Link href={`/${lang}/vinos`}>
           <li className="flex-1 mr-2 cursor-pointer">
             {router.asPath === `/${lang}/vinos` ? (
-              <a className="text-center block border border-gray-900 rounded bg-gray-900 hover:border-gray-700 hover:bg-gray-700 text-white py-2 px-4">
-                {lang === 'en' ? 'WINES' : 'VINOS'}
+              <a className="text-center block border border-gray-900 rounded bg-gray-900 hover:border-gray-700 hover:bg-gray-700 text-white py-2 px-4 h-10">
+                <svg
+                  className="fill-current h-full w-full justify-center"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9 18v-7L0 2V0h20v2l-9 9v7l5 1v1H4v-1l5-1zm2-10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                </svg>
               </a>
             ) : (
-              <a className="text-center block border border-gray-500 rounded hover:border-gray-700 text-gray-700 hover:text-gray-900 hover:bg-gray-300 py-2 px-4">
-                {lang === 'en' ? 'WINES' : 'VINOS'}
+              <a className="text-center block border border-gray-500 rounded hover:border-gray-700 text-gray-700 hover:text-gray-900 hover:bg-gray-300 py-2 px-4 h-10">
+                <svg
+                  className="fill-current h-full w-full justify-center"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9 18v-7L0 2V0h20v2l-9 9v7l5 1v1H4v-1l5-1zm2-10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                </svg>
               </a>
             )}
           </li>
@@ -29,12 +41,24 @@ export default function Nav() {
         <Link href={`/${lang}/platillos`}>
           <li className="flex-1 mr-2 cursor-pointer">
             {router.asPath === `/${lang}/vinos` ? (
-              <a className="text-center block border border-gray-500 rounded hover:border-gray-700 text-gray-700 hover:text-gray-900 hover:bg-gray-300 py-2 px-4">
-                {lang === 'en' ? 'DISHES' : 'PLATILLOS'}
+              <a className="text-center block border border-gray-500 rounded hover:border-gray-700 text-gray-700 hover:text-gray-900 hover:bg-gray-300 py-2 px-4 h-10">
+                <svg
+                  className="fill-current h-full w-full justify-center"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M18 11v7a2 2 0 0 1-4 0v-5h-2V3a3 3 0 0 1 3-3h3v11zM4 10a2 2 0 0 1-2-2V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v7a2 2 0 0 1-2 2v8a2 2 0 0 1-4 0v-8z" />
+                </svg>
               </a>
             ) : (
-              <a className="text-center block border border-gray-900 rounded bg-gray-900 hover:border-gray-700 hover:bg-gray-700 text-white py-2 px-4">
-                {lang === 'en' ? 'DISHES' : 'PLATILLOS'}
+              <a className="text-center block border border-gray-900 rounded bg-gray-900 hover:border-gray-700 hover:bg-gray-700 text-white py-2 px-4 h-10">
+                <svg
+                  className="fill-current h-full w-full justify-center"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M18 11v7a2 2 0 0 1-4 0v-5h-2V3a3 3 0 0 1 3-3h3v11zM4 10a2 2 0 0 1-2-2V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v4h1V1a1 1 0 0 1 2 0v7a2 2 0 0 1-2 2v8a2 2 0 0 1-4 0v-8z" />
+                </svg>
               </a>
             )}
           </li>
